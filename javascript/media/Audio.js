9501b65074d92600a502e748064ae0037bdc2452
Cuong Tran <cuongtran3001@gmail.com>
2013-08-24 22:56:23 +0700

    initialize

---
+++ javascript/media/Audio.js
@@ -0,0 +1,23 @@
+var Audio = {
+    plugin : null
+};
+
+Audio.init = function() {
+    var success = true;
+    
+    this.plugin = document.getElementById("pluginAudio"); 
+    
+    if (!this.plugin) {
+        success = false;
+    }
+    
+    return success;
+};
+
+Audio.setRelativeVolume = function(delta) {
+    this.plugin.SetVolumeWithKey(delta);
+};
+
+Audio.getVolume = function() {
+    return this.plugin.GetVolume();
+};
---
+++ javascript/utils/Event.js
@@ -0,0 +1,26 @@
+var Event = {
+
+	INIT_FISH		: 'initFish',
+		
+	INIT_VIP		: 'initVIP',
+	
+	SELECT_BG		: 'selectBG',
+	
+	SELECT_FISH		: 'selectFish',
+	
+	CHANGE_BG		: 'changeBG',
+	
+	SELECT			: 'select',
+	
+	INIT			: 'init',
+	
+	CHANGE			: 'change',
+	
+	COMPLETE		: 'complete',
+	
+	ERROR			: 'error',
+	
+	RETURN			: 'return',
+	
+	EXIT			: 'exit'
+};
